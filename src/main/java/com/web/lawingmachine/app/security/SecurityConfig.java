2de0876a92400fdb2865998723c72a9a5587ef21
narafu <67089503+narafu@users.noreply.github.com>
2021-12-30 10:34:43 +0900

    Update SecurityConfig.java

--- src/main/java/com/web/lawingmachine/app/security/SecurityConfig.java
+++ src/main/java/com/web/lawingmachine/app/security/SecurityConfig.java
@@ -25,24 +25,29 @@ import static com.web.lawingmachine.app.security.SocialType.*;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // 커스텀한 OAuth2UserService DI.
     @Autowired
     private CustomOAuth2UserService customOAuth2UserService;
 
+	// encoder를 빈으로 등록.
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // WebSecurity에 필터를 거는 게 훨씬 빠름. HttpSecrity에 필터를 걸면, 이미 스프링 시큐리티 내부에 들어온 상태기 때문에..
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/members/**","/image/**");    // /image/** 있는 모든 파일들은 시큐리티 적용을 무시한다.
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());    // 정적인 리소스들에 대해서 시큐리티 적용 무시.
     }
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeRequests()
-            .antMatchers("/", "/index", "/reg", "/oauth2/**", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**", "/favicon.ico/**").permitAll()
+            .antMatchers("/", "/index", "/login/**", "/reg", "/oauth2/**", "/css/**", "/images/**", "/js/**", "/console/**", "/favicon.ico/**").permitAll()
             .antMatchers("/admin/**").hasRole("ADMIN")
-            .antMatchers("/facebook").hasAuthority(FACEBOOK.getRoleType())
-            .antMatchers("/google").hasAuthority(GOOGLE.getRoleType())
-            .antMatchers("/kakao").hasAuthority(KAKAO.getRoleType())
-            .antMatchers("/naver").hasAuthority(NAVER.getRoleType())
             .anyRequest().authenticated()
         .and()
             .oauth2Login()
