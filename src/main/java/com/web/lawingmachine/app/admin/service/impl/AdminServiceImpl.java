c5910dae6e632bfdab0faac2c5b50840bc5bf1bd
narafu <narafu@kakao.com>
2022-01-13 00:52:16 +0900

    빌보드 관련 오류 수정

--- src/main/java/com/web/lawingmachine/app/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/web/lawingmachine/app/admin/service/impl/AdminServiceImpl.java
@@ -15,75 +15,77 @@ import java.util.List;
 @Service
 public class AdminServiceImpl implements AdminService {
 
-    @Autowired
-    private QuizMstrInfoMapper quizMstrInfoMapper;
+	@Autowired
+	private QuizMstrInfoMapper quizMstrInfoMapper;
 
-    @Autowired
-    private QuizDtlInfoMapper quizDtlInfoMapper;
+	@Autowired
+	private QuizDtlInfoMapper quizDtlInfoMapper;
 
-    @Autowired
-    private UserInfoMapper userInfoMapper;
+	@Autowired
+	private UserInfoMapper userInfoMapper;
 
-    @Override
-    public List<QuizMstrInfoVO> selectQuizList(QuizMstrInfoVO quizMstrInfoVO) {
-        return quizMstrInfoMapper.selectQuizList(quizMstrInfoVO);
-    }
+	@Override
+	public List<QuizMstrInfoVO> selectQuizList(QuizMstrInfoVO quizMstrInfoVO) {
+		quizMstrInfoVO.setOffset(quizMstrInfoVO.getOffset() * 10);
+		return quizMstrInfoMapper.selectQuizList(quizMstrInfoVO);
+	}
 
-    @Override
-    public int getQuizListCnt(QuizMstrInfoVO quizMstrInfoVO) {
-        return quizMstrInfoMapper.getQuizListCnt(quizMstrInfoVO);
-    }
+	@Override
+	public int getQuizListCnt(QuizMstrInfoVO quizMstrInfoVO) {
+		return quizMstrInfoMapper.getQuizListCnt(quizMstrInfoVO);
+	}
 
-    @Override
-    public QuizMstrInfoVO getQuizInfo(QuizMstrInfoVO quizMstrInfoVO) {
-        return quizMstrInfoMapper.getQuizInfo(quizMstrInfoVO);
-    }
+	@Override
+	public QuizMstrInfoVO getQuizInfo(QuizMstrInfoVO quizMstrInfoVO) {
+		return quizMstrInfoMapper.getQuizInfo(quizMstrInfoVO);
+	}
 
-    @Override
-    public int insertQuizMstrInfo(QuizMstrInfoVO param) {
-        int resultCnt = quizMstrInfoMapper.insertQuizMstrInfo(param);
-        if (resultCnt > 0) {
-            for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
-                quizDtlInfoMapper.insertQuizDtlInfo(quizDtlInfoVO);
-            }
-        }
-        return resultCnt;
-    }
+	@Override
+	public int insertQuizMstrInfo(QuizMstrInfoVO param) {
+		int resultCnt = quizMstrInfoMapper.insertQuizMstrInfo(param);
+		if (resultCnt > 0) {
+			for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
+				quizDtlInfoMapper.insertQuizDtlInfo(quizDtlInfoVO);
+			}
+		}
+		return resultCnt;
+	}
 
-    @Override
-    public int updateQuizMstrInfo(QuizMstrInfoVO param) {
-        int resultCnt = quizMstrInfoMapper.updateQuizMstrInfo(param);
-        if (resultCnt > 0) {
-            for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
-                quizDtlInfoMapper.updateQuizDtlInfo(quizDtlInfoVO);
-            }
-        }
-        return resultCnt;
-    }
+	@Override
+	public int updateQuizMstrInfo(QuizMstrInfoVO param) {
+		int resultCnt = quizMstrInfoMapper.updateQuizMstrInfo(param);
+		if (resultCnt > 0) {
+			for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
+				quizDtlInfoMapper.updateQuizDtlInfo(quizDtlInfoVO);
+			}
+		}
+		return resultCnt;
+	}
 
-    @Override
-    public int delQuizMstrInfo(QuizMstrInfoVO param) {
-        int resultCnt = quizMstrInfoMapper.delQuizMstrInfo(param);
-        if (resultCnt > 0) {
-            for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
-                quizDtlInfoMapper.delQuizDtlInfo(quizDtlInfoVO);
-            }
-        }
-        return resultCnt;
-    }
+	@Override
+	public int delQuizMstrInfo(QuizMstrInfoVO param) {
+		int resultCnt = quizMstrInfoMapper.delQuizMstrInfo(param);
+		if (resultCnt > 0) {
+			for (QuizDtlInfoVO quizDtlInfoVO : param.getQuizDtlList()) {
+				quizDtlInfoMapper.delQuizDtlInfo(quizDtlInfoVO);
+			}
+		}
+		return resultCnt;
+	}
 
-    @Override
-    public List<UserInfoVO> selectApprovalList(UserInfoVO param) {
-        return userInfoMapper.selectApprovalList(param);
-    }
+	@Override
+	public List<UserInfoVO> selectApprovalList(UserInfoVO param) {
+		param.setOffset(param.getOffset() * 10);
+		return userInfoMapper.selectApprovalList(param);
+	}
 
-    @Override
-    public int getApprovalListCnt(UserInfoVO param) {
-        return userInfoMapper.getApprovalListCnt(param);
-    }
+	@Override
+	public int getApprovalListCnt(UserInfoVO param) {
+		return userInfoMapper.getApprovalListCnt(param);
+	}
 
-    @Override
-    public int updateMembershipCd(String userId, String membershipCd) {
-        return userInfoMapper.updateMembershipCd(userId, membershipCd);
-    }
+	@Override
+	public int updateMembershipCd(String userId, String membershipCd) {
+		return userInfoMapper.updateMembershipCd(userId, membershipCd);
+	}
 }
