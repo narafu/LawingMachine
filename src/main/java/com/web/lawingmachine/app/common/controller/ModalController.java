68088133697e73796086d0b6d4096921883e6e43
narafu <narafu@kakao.com>
2022-01-17 01:43:21 +0900

    오류수정 등

--- src/main/java/com/web/lawingmachine/app/common/controller/ModalController.java
+++ src/main/java/com/web/lawingmachine/app/common/controller/ModalController.java
@@ -14,23 +14,17 @@ public class ModalController {
         model.addAttribute("modalVO", modalVO);
         return "view/modal/alert";
     }
-    
+
     @RequestMapping("/confirm")
     public String confirm(ModalVO modalVO, ModelMap model) {
-    	model.addAttribute("modalVO", modalVO);
-    	return "view/modal/confirm";
+        model.addAttribute("modalVO", modalVO);
+        return "view/modal/confirm";
     }
 
     @RequestMapping("/dialog")
     public String dialog(ModalVO modalVO, ModelMap model) {
         model.addAttribute("modalVO", modalVO);
         return "view/modal/dialog";
-    }
-
-    @RequestMapping("/quizStartConfirm")
-    public String quizStartConfirm(ModalVO modalVO, ModelMap model) {
-        model.addAttribute("modalVO", modalVO);
-        return "view/modal/quizStartConfirm";
     }
 
     @RequestMapping("/quizResultConfirm")
