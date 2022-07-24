b5909e7cc08b21def2c0ba7f03b45f9985b06af9
narafu <67089503+narafu@users.noreply.github.com>
2022-07-24 17:23:16 +0900

    데이터 초기화 수정

--- src/main/java/com/web/lawingmachine/app/common/entity/CmmnDtlCdEntity.java
+++ src/main/java/com/web/lawingmachine/app/common/entity/CmmnDtlCdEntity.java
@@ -13,17 +13,17 @@ public class CmmnDtlCdEntity extends BaseEntity {
     @GeneratedValue
     private int cmmnDtlCdSeq;
 
-    @Column(length = 3)
-    private char grpCd; // 001: exam, 002: subject, 003: board, 004: admin
+    @Column(length = 10)
+    private String grpCd; // 001: exam, 002: subject, 003: board, 004: admin, 005: membership ...
 
-    @Column(length = 2)
-    private char grpDtlCd;
+    @Column(length = 10)
+    private String grpDtlCd; // 10, 20, 99 ...
 
     private String grpDtlNm;
 
     private int srtNo;
 
     @Column(length = 1)
-    private char useYn;
+    private char useYn; // Y, N
 
 }
